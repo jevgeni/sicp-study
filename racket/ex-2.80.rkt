af04874d5395e8dafd9b3bb26ed4cb7c4e14f801
Jevgeni Holodkov <jevgeni.holodkov@gmail.com>
2011-01-10 17:02:04 +0200

    ex 2.79/2.80

---
+++ racket/ex-2.80.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+(put '=zero? '(scheme-number) (lambda (x) (= x 0)))
+(put '=zero? '(rational) (lambda (x) (and (= (numer x) 0) (not (= (denom x))))))
+(put '=zero? '(complex) (lambda (x) (and (= (real-part x) 0) (= (imag-part x) 0)))) ; or go down to rectangular/polar 
